4d1072e0ce8f0a9eb6eabee1f27f4df5af917831
Vasaluk <101427439+Vasaluk@users.noreply.github.com>
2022-06-02 20:53:43 +0300

    Add files via upload

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImp.java
@@ -0,0 +1,39 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional
+public class RoleServiceImp implements RoleService {
+
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImp(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+        saveRole(new Role(1L, "ADMIN"));
+        saveRole(new Role(2L, "USER"));
+    }
+
+    @Override
+    public void saveRole(Role role) {
+        roleRepository.save(role);
+    }
+
+    @Override
+    public Set<Role> getRole(List<Long> id) {
+        return new HashSet<>(roleRepository.findAllById(id));
+    }
+
+    public List<Role> listAll() {
+        return roleRepository.findAll();
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,57 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImp implements UserDetailsService, UserService {
+    UserRepository userRepository;
+
+    BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImp(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public List<User> listAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
